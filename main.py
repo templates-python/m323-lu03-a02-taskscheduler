01985f3e9bd2dffe9cb5bfbc1ca09afb952c7828
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 11:33:25 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+# define your tasks here
 
+def task_scheduler(tasks, delay):
+    # your code goes here
+    pass
 
 if __name__ == '__main__':
-    calculate()
+    tasks = []
+    delay = 2
+    task_scheduler(tasks, delay)
--- main_test.py
+++ main_test.py
@@ -1,6 +1,36 @@
+import time
 import main
+def test_task_execution_order():
+    executed_tasks = []
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+    def task1():
+        executed_tasks.append('Task 1 executed!')
+
+    def task2():
+        executed_tasks.append('Task 2 executed!')
+
+    tasks = [task1, task2]
+    delay = 1
+
+    main.task_scheduler(tasks, delay)
+
+    assert executed_tasks == ['Task 1 executed!', 'Task 2 executed!'], f'Expected ["Task 1 executed!", "Task 2 executed!"], got {executed_tasks}'
+
+def test_task_execution_with_delay():
+    def task1():
+        pass
+
+    def task2():
+        pass
+
+    tasks = [task1, task2]
+    delay = 1  # 1 second delay
+
+    start_time = time.time()
+    main.task_scheduler(tasks, delay)
+    end_time = time.time()
+
+    elapsed_time = end_time - start_time
+
+    # Überprüfen, ob die tatsächlich verstrichene Zeit ungefähr der erwarteten Zeit entspricht.
+    assert abs(elapsed_time - 2.0) < 0.1, f'Expected around 2.0 seconds, got {elapsed_time} seconds'
