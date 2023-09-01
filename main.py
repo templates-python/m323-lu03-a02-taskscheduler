aa22062927545eebec5cec15f5a6fa637d1d95b6
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 11:41:11 +0200

    Added Expert

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def task_scheduler(tasks, delay):
     # your code goes here
     pass
 
+def task_scheduler_expert(tasks, delays):
+    # your code goes here
+    pass
+
 if __name__ == '__main__':
     tasks = []
     delay = 2
+    delays = [2,3]
     task_scheduler(tasks, delay)
--- main_test.py
+++ main_test.py
@@ -34,3 +34,44 @@ def test_task_execution_with_delay():
 
     # Überprüfen, ob die tatsächlich verstrichene Zeit ungefähr der erwarteten Zeit entspricht.
     assert abs(elapsed_time - 2.0) < 0.1, f'Expected around 2.0 seconds, got {elapsed_time} seconds'
+
+def test_task_execution_order_expert():
+    executed_tasks = []
+
+    def task1():
+        executed_tasks.append('Task 1 executed!')
+
+    def task2():
+        executed_tasks.append('Task 2 executed!')
+
+    tasks = [task1, task2]
+    delays = [1, 2]
+
+    main.task_scheduler_expert(tasks, delays)
+
+    assert executed_tasks == ['Task 1 executed!', 'Task 2 executed!'], f'Expected ["Task 1 executed!", "Task 2 executed!"], got {executed_tasks}'
+
+def test_task_execution_with_individual_delays():
+    executed_tasks = []
+
+    def task1():
+        executed_tasks.append((1, time.time()))
+
+    def task2():
+        executed_tasks.append((2, time.time()))
+
+    tasks = [task1, task2]
+    delays = [1, 2]
+
+    start_time = time.time()
+    main.task_scheduler_expert(tasks, delays)
+    end_time = time.time()
+
+    elapsed_time_task1 = executed_tasks[0][1] - start_time
+    elapsed_time_task2 = executed_tasks[1][1] - start_time
+
+    # Überprüfen, ob die tatsächlich verstrichene Zeit ungefähr der erwarteten Zeit entspricht.
+    assert abs(elapsed_time_task1 - 1) < 0.1, f'Expected around 1 second for task 1, got {elapsed_time_task1} seconds'
+    assert abs(elapsed_time_task2 - (1 + 2)) < 0.1, f'Expected around 3 seconds for task 2, got {elapsed_time_task2} seconds'
+
+
