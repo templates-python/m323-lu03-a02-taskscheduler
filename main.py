9250868b9408d48ec9b6b8a30ad8683be5e03017
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-01 11:34:36 +0200

    done

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-# define your tasks here
+import time
+def task1():
+    print('Task 1 executed!')
+
+def task2():
+    print('Task 2 executed!')
 
 def task_scheduler(tasks, delay):
-    # your code goes here
-    pass
+    for task in tasks:
+        task()
+        time.sleep(delay)
 
 if __name__ == '__main__':
-    tasks = []
+    tasks = [task1, task2]
     delay = 2
     task_scheduler(tasks, delay)
