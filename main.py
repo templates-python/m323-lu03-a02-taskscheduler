e129b60e82738b3287312b6745a51e9b4fd0d267
graphics80 <kevin.maurizi@bzz.ch>
2023-09-06 15:57:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-# define your tasks here
-
+def task1():
+    """
+    Eine einfache Funktion, die eine Meldung ausgibt, um zu signalisieren, dass Task 1 ausgeführt wurde.
+    """
+    pass
+ 
+ 
+def task2():
+    """
+    Eine einfache Funktion, die eine Meldung ausgibt, um zu signalisieren, dass Task 2 ausgeführt wurde.
+    """
+    pass
+ 
+ 
 def task_scheduler(tasks, delay):
-    # your code goes here
+    """
+    Führt eine Liste von Tasks mit einer festen Zeitverzögerung zwischen jedem Task aus.
+ 
+    Args:
+    tasks (list): Eine Liste von Funktionen, die ausgeführt werden sollen.
+    delay (int): Die Zeitverzögerung in Sekunden zwischen den einzelnen Tasks.
+ 
+    Returns:
+    None
+    """
     pass
-
+ 
 def task_scheduler_expert(tasks, delays):
-    # your code goes here
+    """
+    Führt eine Liste von Tasks mit unterschiedlichen Zeitverzögerungen zwischen jedem Task aus.
+ 
+    Args:
+    tasks (list): Eine Liste von Funktionen, die ausgeführt werden sollen.
+    delays (list): Eine Liste von Zeitverzögerungen in Sekunden für die jeweiligen Tasks.
+ 
+    Returns:
+    None
+    """    
     pass
-
+ 
 if __name__ == '__main__':
     tasks = [task1, task2]
     delay = 2
