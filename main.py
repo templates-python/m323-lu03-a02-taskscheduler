63c4011853cf07c90e89276260b27acf07e20b8b
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:49 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import time
+
+
 def task1():
     print('Task 1 executed!')
 
+
 def task2():
     print('Task 2 executed!')
+
 
 def task_scheduler(tasks, delay):
     for task in tasks:
         task()
         time.sleep(delay)
 
+
 def task_scheduler_expert(tasks, delays):
     for task, delay in zip(tasks, delays):
         task()
         time.sleep(delay)
 
+
 if __name__ == '__main__':
     tasks = [task1, task2]
     delay = 2
-    delays = [2,3]
+    delays = [2, 3]
     task_scheduler(tasks, delay)
     task_scheduler_expert(tasks, delays)
